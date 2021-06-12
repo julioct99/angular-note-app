bc1f122f5693aa47b100ba5368595905f697fe8c
julioct99 <jcasallot01@gmail.com>
2021-06-12 20:49:54 +0200

    Basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Note } from './shared/models/note.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-note-app';
+
+  editedNote: Note;
+  notes: Note[] = [new Note(1, 'Test', 'Test', new Date())];
+
+  constructor() {}
 }
--- src/app/notes.service.ts
+++ src/app/notes.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Note } from './shared/models/note.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NotesService {
-  notes = [];
+  notes: Note[] = [new Note(1, 'Test', 'Test', new Date())];
 
   constructor() {}
+
+  getNotes() {
+    return [...this.notes];
+  }
 }
